46530d29fabb978a21f1a6d5aa43e99f645c818a
Nadine <trauflernadine@gmail.com>
2026-01-22 14:52:07 +0100

    day 2 part 2

--- 2025/day2/day2_part2.py
+++ 2025/day2/day2_part2.py
@@ -4,15 +4,37 @@ with open('./day2.txt') as f:
     ranges = f.read().split(",")
 sum_of_all_invalid_ids = 0
 
+def is_repetitive(id_str, number_of_repetitions):
+    start = 0
+    sample_size = len(id_str)//number_of_repetitions
+    # print("id: " + str(id_str) + " repet: "+ str(number_of_repetitions ) + " size: "+ str(sample_size))
+    if sample_size == 1:
+        for ind in range(number_of_repetitions - 1):
+            i = ind * sample_size
+            j = (ind + 1) * sample_size
+            # print(id_str[i] )
+            if id_str[i] != id_str[j]:
+                return False
+    else:
+        for ind in range(number_of_repetitions - 1):
+            i = ind * sample_size
+            j = (ind + 1) * sample_size
+            # print(id_str[j : j + sample_size] )
+            if id_str[i :j] != id_str[j:j + sample_size]:
+                return False
+    return True
+
+
 def is_id_invalid(id):
     id_str = str(id)
     id_length = len(id_str)
 
-    if id_length % 2 == 0:
-        if (id_str[:int(id_length/2)] == id_str[int(id_length/2):]):
-            print("id length " + str(id_length))
-            print("id invalid: " + id_str)
-            return True
+    for i in range(2,  id_length+1):
+        if id_length % i == 0:
+            if (is_repetitive(id_str, i)):
+                # print("id length " + str(id_length) + " i: " + str(i))
+                print("id invalid: " + id_str)
+                return True
     else:
         return False
 
