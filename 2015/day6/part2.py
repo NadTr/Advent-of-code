21cb86999735ef6ee47a5b5c71364f48d70d53cb
Nadine <trauflernadine@gmail.com>
2026-01-30 13:22:36 +0100

    2015 day 6 part 2

---
+++ 2015/day6/part2.py
@@ -0,0 +1,31 @@
+import numpy as np
+import re
+print("advent of code 2015 day 6")
+
+with open('./input.txt') as f:
+    file = f.read().splitlines()
+
+lights = np.zeros((1000,1000),  dtype=int)
+
+def change_lights(area, manner):
+    for x in range(int(area[0][0]), int(area[1][0]) + 1):
+        for y in range(int(area[0][1]), int(area[1][1]) + 1):
+            if manner == "toggle ":
+                lights[x,y] += 2
+            elif manner == "turn on ":
+                lights[x,y] += 1
+            else:
+                lights[x,y] -= 1 if lights[x,y] > 0 else 0
+
+for line in file : 
+    manner = re.findall('([a-zA-Z ]*)\d*.*', line)
+    area = line[len(manner[0]):].split("through")
+    for i in range(len(area)):
+        area[i] = re.findall(r'-?\d*\.?\d+', area[i])
+    # print (manner, " on ", area)
+    change_lights(area, manner[0])
+
+    
+number_of_ligths_on = sum(sum(lights))
+
+print("The number of ligths on is ", number_of_ligths_on)
