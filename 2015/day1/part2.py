61522ddc6f21a5a6870fc1bfd1ccc285ba7fe245
Nadine <trauflernadine@gmail.com>
2026-01-30 09:34:04 +0100

    2015 day 1 part 2

---
+++ 2015/day1/part2.py
@@ -0,0 +1,15 @@
+print("advent of code 2015 day 1")
+
+with open('./input.txt') as f:
+    file = f.read()
+
+level = 0
+
+for step in range(len(file)):
+    if file[step] == "(":
+        level += 1
+    elif file[step] == ")":
+        level -= 1
+    if(level == -1):
+        print("the first time in the level -1 is on step", step + 1)
+        break
