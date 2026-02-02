9f7d9bc11d4a71bdaf5c0a0d6215883c2fe06432
Nadine <trauflernadine@gmail.com>
2026-02-02 11:50:14 +0100

    2015 day 11 part 2

--- 2015/day11/part2.py
+++ 2015/day11/part2.py
@@ -1,18 +1,15 @@
-import string, re
+import string
 print("advent of code 2015 day 11")
 alphabet =  string.ascii_lowercase
-isNotValid = True
+
 with open('./input.txt') as f:
     password = f.read()
 
-
 def increment_letter(letter):
     if letter == "z":
-        # print("passage de z à a")
         return "a", True
     elif letter == "h" or letter == "k" or letter == "n":
         return alphabet[ alphabet.index(letter) + 2], False
-
     else : 
         return alphabet[ alphabet.index(letter) + 1], False
  
@@ -29,11 +26,7 @@ def increment_password(password):
 
 def is_password_valid(password):
     contain_3_straight_letters = False
-    contain_double_letters = 0
-    if "i" in password or "l" in password or "o" in password:
-        # print("The password ", password, " contains one of these letters :  i, l, o")
-        return True
-    
+    contain_double_letters = 0    
     for i in range(len(alphabet)) :
         letter = alphabet[i]
         if letter + letter in password:
@@ -54,13 +47,15 @@ if "i" in password or "l" in password or "o" in password:
             (new_letter, bool) =increment_letter(password[i])
             password = password[0:i] + new_letter + "a" * (len(password)-i)
             break
+
 print(password)
 new_password = list(password)
 
-while(isNotValid):
-    new_password = increment_password(password)
-    isNotValid = is_password_valid(new_password)
-    password = new_password
-
+for i in range(2):
+    isNotValid = True
+    while(isNotValid):
+        new_password = increment_password(password)
+        isNotValid = is_password_valid(new_password)
+        password = new_password
 
 print("The new valid password is ", new_password)
