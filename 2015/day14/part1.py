476f159e94508a7e795c78a14610d563d8adce59
Nadine <trauflernadine@gmail.com>
2026-02-03 09:27:57 +0100

    2015 day 14 part 1

---
+++ 2015/day14/part1.py
@@ -0,0 +1,39 @@
+print("advent of code 2015 day 14")
+
+run_time = 2503
+datas = {}
+biggest_distance = 0
+all_distances = []
+
+with open('./input.txt') as f:
+    file = f.read().splitlines()
+
+def run(reindeer, time):
+    timer = 0
+    distance = 0
+    # while(timer <= time):
+    for i in range(time):
+        if timer < reindeer["run_time"]:
+            distance += reindeer["speed"]
+            # print(i, timer, distance)
+            timer +=1
+        elif timer >= reindeer["run_time"] and timer < reindeer["run_time"] +reindeer["rest_time"]:
+            timer += 1
+        else:
+            timer = 1
+            distance += reindeer["speed"]
+    return distance
+
+
+for i in range(len(file)):
+    line = file[i].split() 
+    datas[line[0]]={ "speed": int(line[3]), "run_time": int(line[6]), "rest_time": int(line[13])}
+
+for key in datas:
+    distance = run(datas[key], run_time)
+    all_distances += [distance]
+
+# print(all_distances)
+biggest_distance = max(all_distances)
+
+print("The winning reinder has run a total of ", biggest_distance)
