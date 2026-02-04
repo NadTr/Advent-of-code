be664350c232dc91f6eefbf548057cb8e841f3c9
Nadine <trauflernadine@gmail.com>
2026-02-04 12:51:53 +0100

    2016 day 3 part 1

---
+++ 2016/day3/part1.py
@@ -0,0 +1,23 @@
+print("advent of code 2016 day 3")
+
+with open('./input.txt') as f:
+    file = f.read().splitlines()
+
+number_of_possible_triangle = 0
+
+def is_triangle_possible(triangle):
+    c1 = int(triangle[0])        
+    c2 = int(triangle[1])        
+    c3 = int(triangle[2])
+    if c1 + c2 > c3 and    c3 + c1 > c2 and  c2 + c3 > c1:
+        return True
+    else:
+        return False 
+    
+
+for line in file:
+    triangle = line.split()
+    if(is_triangle_possible(triangle)):
+        number_of_possible_triangle += 1
+
+print("The number of possible traingles is ", number_of_possible_triangle)
