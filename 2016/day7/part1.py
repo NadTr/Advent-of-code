70caa9f567d7b8cc1e47af5c2d85ba28caad0517
Nadine <trauflernadine@gmail.com>
2026-02-05 10:40:26 +0100

    2016 day 7 part 1

---
+++ 2016/day7/part1.py
@@ -0,0 +1,57 @@
+print("advent of code 2016 day 7")
+
+with open('./input.txt') as f:
+    file = f.read().splitlines()
+
+number_of_valid_ips = 0
+
+def is_ip_valid(ip):
+    ip_chars = list(ip)
+    inside_brakets = False
+    one_ip_valid = False
+    for i in range(len(ip_chars)):
+        if ip_chars[i]=="[":
+            inside_brakets = True
+        elif ip_chars[i] == "]":
+            inside_brakets = False
+        if i < len(ip_chars) -3:
+            if "[" not in ip_chars[i:i+4] and "]" not in ip_chars[i:i+4]:
+                if ip_chars[i] != ip_chars[i+1] and ip_chars[i]+ip_chars[i+1] == ip_chars[i+3]+ip_chars[i+2]:
+                    if inside_brakets:
+                        if "]" in ip_chars[i:]:
+                            print("ip not valid", i, i+4, ip[i:i+4])
+                            return False
+                        else: 
+                            one_ip_valid = True
+                            print("ip not have end ]", ip[i:])
+                    else:
+                        print("found one abba not inside brakets", ip[i:i+4], ip)
+                        one_ip_valid = True
+    return one_ip_valid
+
+    # ip_chars = list(ip)
+    # one_ip_valid = False
+    # for i in range(len(ip_chars)):
+    #     if i < len(ip_chars) -3:
+    #         if "[" not in ip_chars[i:i+4] and "]" not in ip_chars[i:i+4]:
+    #             # if ip_chars[i] != ip_chars[i+1] and ip_chars[i:i+2] == ip_chars[i+2:i+4]:
+    #             if ip_chars[i] != ip_chars[i+1] and ip_chars[i]+ip_chars[i+1] == ip_chars[i+3]+ip_chars[i+2]:
+    #                 if i > 0 and  i+4 < len(ip_chars) and (ip_chars[i-1] == "[" and ip_chars [i+4] =="]"):
+    #                     i += 5
+    #                     print(i, i+5, ip[i-1:i+5])
+    #                     return False
+    #                 else:
+    #                     print(i, i +4, ip[i:i+4])
+    #                     i += 4
+    #                     one_ip_valid = True
+    # return one_ip_valid
+
+
+for line in file:
+    if is_ip_valid(line):
+        number_of_valid_ips += 1
+
+
+
+
+print("The number of valid ips is ", number_of_valid_ips)
