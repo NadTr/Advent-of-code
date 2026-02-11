0caac72219bfbceb308ba53212849544aed1ea49
Nadine <trauflernadine@gmail.com>
2026-02-11 11:14:27 +0100

    2015 day 17 part 1

---
+++ 2015/day17/part1.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+import itertools
+print("advent of code 2015 day 17")
+
+script_location = Path(__file__).absolute().parent
+with open(script_location /'input.txt') as f:
+      file = f.read().splitlines()
+
+working_combinations = 0
+number_to_reach = 150
+            
+for i in range(len(file)):
+    file[i] = int(file[i])
+
+for i in range(len(file)):
+    combinaisons = itertools.combinations(file,i)
+    for combi in combinaisons:
+         if sum(combi) == number_to_reach:
+              working_combinations += 1
+
+print("The number combinations with the recipients is ", working_combinations)
