1d190fa6e7cae43232e958c92b64c653d340329b
Nadine <trauflernadine@gmail.com>
2026-02-16 17:07:37 +0100

    2024 day2 part 2

--- 2024/day2/day2_part2.py
+++ 2024/day2/day2_part2.py
@@ -7,25 +7,31 @@ with open(script_location /'input.txt') as f:
 
 number_of_safe_reports = 0
 
-def is_report_safe(report):
+def is_report_safe(report, safety_number):
     if abs(report[1] - report[0]) > 0:
         sign = "+" if report[1] > report[0] else "-"
         for i in range(len(report)-1):
             result = report[i + 1] - report[i] if sign == "+" else report[i] - report[i + 1]
-            # print("result : ", result)
             if result > 0 and result < 4:
                 continue
             else :
-                return 0
+                if safety_number == 1:
+                    return 0
+                else:
+                    new_report1 = report[:i]+report[i+1:]
+                    new_report2 = report[:i + 1]+report[i+2:]
+                    new_report3 = report[:i -1]+report[i:]
+                    return is_report_safe(new_report1, 1) + is_report_safe(new_report2, 1)+ is_report_safe(new_report3, 1)
+        
         return 1
     else:
-        return 0
-    
+        return 0 if safety_number == 1 else is_report_safe(report[1:], 1)
+        
 
 for line in file:
     report = line.split()
     for i in range(len(report)):
         report[i] = int(report[i])
-    number_of_safe_reports += is_report_safe(report)
+    number_of_safe_reports +=  1 if is_report_safe(report, 0) >=1  else 0
 
-print("The total of safe reports is " + str(number_of_safe_reports))
+print("The total of safe reports is ", number_of_safe_reports)
