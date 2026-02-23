3685abc29f6a064571e675276b6e414c4d64507f
Nadine <trauflernadine@gmail.com>
2026-02-23 09:07:42 +0100

    2019 day1 part1

---
+++ 2019/day1/part1.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+print("advent of code 2018 day 1")
+
+script_location = Path(__file__).absolute().parent
+with open(script_location /'input.txt') as f:
+     file = f.read().splitlines()
+
+def get_fuel_requirement(mass):
+    return int(mass)//3 - 2
+
+total_fuel = 0
+for mass in file:
+    total_fuel += get_fuel_requirement(mass)
+
+print("The rsum of the fuel requirements for all of the modules", total_fuel)
