5575733caa89826c68881f437e10e86d79c56959
Nadine <trauflernadine@gmail.com>
2026-02-23 11:28:48 +0100

    2019 day4 part1

---
+++ 2019/day4/part1.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+print("advent of code 2019 day 4")
+
+script_location = Path(__file__).absolute().parent
+with open(script_location /'input.txt') as f:
+    file = f.read().split("-")
+
+def is_password_valid(password):
+    double = False
+    increase = True
+    for i in range(len(password) - 1):
+        if password[i] == password[i+1]:
+            double = True
+        if int(password[i]) > int(password[i+1]):
+            increase = False
+    if double and increase:
+        return True
+    else: return False
+
+valid_passwords = 0
+for password in range(int(file[0]), int(file[1]) +1):
+    if is_password_valid(str(password)):
+        valid_passwords += 1
+
+print("The number of different passwords within the range given is", valid_passwords)
